bb60e6ee496ba92dfcc6ac076198560fbd13aef1
Mahak Malik <mahakmalik91@gmail.com>
2019-05-30 00:17:14 -0400

    test case added

--- DroneDeliverySystem/Test/com/dds/tests/ValidateCalculateNPSScore.java
+++ DroneDeliverySystem/Test/com/dds/tests/ValidateCalculateNPSScore.java
@@ -3,12 +3,12 @@ package com.dds.tests;
 import java.time.LocalTime;
 
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
+
 
 import com.dds.appcode.CalculateNPSScore;
 import com.dds.appcode.OrderDetails;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -17,23 +17,24 @@ public class ValidateCalculateNPSScore {
 
 	OrderDetails order=new OrderDetails("WM001" , "N7E3" , LocalTime.parse("10:00:00"));
 	public LocalTime dispatchTime;
-	
-	@BeforeClass
-	public void resetstatic() {
+		
+	@Before
+	public static void resetStaticVariables() {
 		CalculateNPSScore.promotors=0;
+		CalculateNPSScore.totalProcessedOrders=0;
+		CalculateNPSScore.detractors=0;
 	}
 	
 	@When("^Order details and dispatch time are passed$")
 	public void orderdetails_and_dispatchtime_is_provided_delivery_method() {
-		dispatchTime=LocalTime.parse("10:00:00");		
+		dispatchTime=LocalTime.parse("10:00:00");
 		CalculateNPSScore.calculateNPS(order, LocalTime.parse("10:30:00"), dispatchTime);	
 		CalculateNPSScore.calculateNPS(order, LocalTime.parse("14:00:00"), dispatchTime);
 	}
 	
 	@Then("^The promoter should be incremented according to time elapsed$")
 	public void validate_calculated_promoters() {
-	
-		Assert.assertEquals(4,CalculateNPSScore.promotors);		
+	    Assert.assertEquals(1,CalculateNPSScore.promotors);		
 	}
 	
 	@Then("^Or the detractor should be incremented according to time elapsed$")
@@ -43,7 +44,7 @@ public class ValidateCalculateNPSScore {
 	
 	@And("^Total processedorder should be incremented$")
 	public void validate_calculated_totalprocessedorder() {
-		Assert.assertEquals(6,CalculateNPSScore.totalProcessedOrders);		
+		Assert.assertEquals(2,CalculateNPSScore.totalProcessedOrders);		
 	}
 	
 }
